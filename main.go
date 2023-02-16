4e78a1f6d78ed5f30bfebd673dea654c1fdf4c2b
Armağan Gök <1armagangok@gmail.com>
2023-02-17 01:29:33 +0300

    IMDB Top 250 movies example fetch

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 func main() {
-	print("let's go\n")  
+	fetchTop250MoviesFromImd()
+
+}
+
+func fetchTop250MoviesFromImd() {
+	response, httpError := http.Get("https://www.imdb.com/chart/top")
+
+	if response.StatusCode != 200 {
+		fmt.Println("ERROR: ", response.StatusCode)
+	} else {
+		document, readerError := goquery.NewDocumentFromReader(response.Body)
+		document.Find(".lister-list").Each(func(i int, selection *goquery.Selection) {
+			title := selection.Find("a").Text()
+			fmt.Println(title)
+
+			print(readerError)
+		})
+	}
+
+	print(httpError)
 }
